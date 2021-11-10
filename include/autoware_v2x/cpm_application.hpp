e8238701aeb4a69b43c772a30da2d7e5bbd546b9
Yu Asabe <yuasabe@g.ecc.u-tokyo.ac.jp>
2021-11-11 00:21:32 +0900

    Major updates

--- include/autoware_v2x/cpm_application.hpp
+++ include/autoware_v2x/cpm_application.hpp
@@ -8,56 +8,63 @@
 #include "autoware_perception_msgs/msg/dynamic_object_array.hpp"
 #include "autoware_v2x/positioning.hpp"
 
-class CpmApplication : public Application
+namespace v2x
 {
-public:
-    CpmApplication(rclcpp::Node *node, vanetza::Runtime&);
-    PortType port() override;
-    void indicate(const DataIndication &, UpPacketPtr) override;
-    void set_interval(vanetza::Clock::duration);
-    void updateObjectsStack(const autoware_perception_msgs::msg::DynamicObjectArray::ConstSharedPtr);
-    void updateMGRS(double *, double *);
-    void updateRP(double *, double *, double *);
-    void updateGenerationDeltaTime(int *);
-    void updateHeading(double *);
-    void send();
+    class V2XNode;
+    class CpmApplication : public Application
+    {
+    public:
+        CpmApplication(V2XNode *node, vanetza::Runtime &);
+        PortType port() override;
+        void indicate(const DataIndication &, UpPacketPtr) override;
+        void set_interval(vanetza::Clock::duration);
+        void updateObjectsStack(const autoware_perception_msgs::msg::DynamicObjectArray::ConstSharedPtr);
+        void updateMGRS(double *, double *);
+        void updateRP(double *, double *, double *);
+        void updateGenerationDeltaTime(int *);
+        void updateHeading(double *);
+        void send();
 
-private:
-    void schedule_timer();
-    void on_timer(vanetza::Clock::time_point);
-    
-    rclcpp::Node* node_;
-    vanetza::Runtime& runtime_;
-    vanetza::Clock::duration cpm_interval_;
-    struct Object {
-        int objectID; // 0-255
-        rclcpp::Time timestamp;
-        double position_x;
-        double position_y;
-        double position_z;
-        double orientation_x;
-        double orientation_y;
-        double orientation_z;
-        double orientation_w;
-        int xDistance;
-        int yDistance;
-        double xSpeed;
-        double ySpeed;
-        vanetza::PositionFix position;
-        int timeOfMeasurement;
+        struct Object
+        {
+            int objectID; // 0-255
+            rclcpp::Time timestamp;
+            double position_x;
+            double position_y;
+            double position_z;
+            double orientation_x;
+            double orientation_y;
+            double orientation_z;
+            double orientation_w;
+            int xDistance;
+            int yDistance;
+            double xSpeed;
+            double ySpeed;
+            vanetza::PositionFix position;
+            int timeOfMeasurement;
+        };
+        std::vector<CpmApplication::Object> objectsStack;
+        std::vector<CpmApplication::Object> receivedObjectsStack;
+
+    private:
+        void schedule_timer();
+        void on_timer(vanetza::Clock::time_point);
+
+        V2XNode *node_;
+        vanetza::Runtime &runtime_;
+        vanetza::Clock::duration cpm_interval_;
+
+        double ego_x_;
+        double ego_y_;
+        double ego_lat_;
+        double ego_lon_;
+        double ego_altitude_;
+        double ego_heading_;
+        int generationDeltaTime_;
+
+        bool updating_objects_stack_;
+        bool sending_;
     };
-    std::vector<CpmApplication::Object> objectsStack;
-
-    double ego_x_;
-    double ego_y_;
-    double ego_lat_;
-    double ego_lon_;
-    double ego_altitude_;
-    double ego_heading_;
-    int generationDeltaTime_;
-
-    bool updating_objects_stack_;
-    bool sending_;
-};
+}
 
 #endif /* CPM_APPLICATION_HPP_EUIC2VFR */
--- src/v2x_node.cpp
+++ src/v2x_node.cpp
@@ -25,88 +25,74 @@ using namespace std::chrono;
 
 namespace v2x
 {
-  V2XNode::V2XNode(const rclcpp::NodeOptions &node_options) : rclcpp::Node("autoware_v2x_node", node_options)
-  // io_service_(),
-  //trigger_(io_service_),
-  // runtime_(new Runtime(Clock::at(boost::posix_time::microsec_clock::universal_time()))),
-  // device_name_("wlp4s0"),
-  // device_(device_name_),
-  // mac_address_(device_.address()),
-  // link_layer_(create_link_layer(io_service_, device_, "ethernet")),
-  // positioning_(create_position_provider(io_service_, trigger_.runtime())),
-  // security_(create_security_entity(trigger_.runtime(), *positioning_)),
-  // mib_(),
-  // cp_(new CpmApplication(this, runtime_)),
-  // context_(mib_, trigger_, *positioning_, security_.get()),
-  // app_()
+  V2XNode::V2XNode(const rclcpp::NodeOptions &node_options) : rclcpp::Node("autoware_v2x_node", node_options), received_time(this->now()), cpm_received(false)
   {
     using std::placeholders::_1;
     subscription_ = this->create_subscription<autoware_perception_msgs::msg::DynamicObjectArray>("/perception/object_recognition/objects", 10, std::bind(&V2XNode::objectsCallback, this, _1));
 
     subscription_pos_ = this->create_subscription<tf2_msgs::msg::TFMessage>("/tf", 10, std::bind(&V2XNode::tfCallback, this, _1));
-    RCLCPP_INFO(get_logger(), "V2X Node Launched");
 
-    // device_name_ = "wlp4s0";
-    // device_(device_name_);
-    // mac_address_ = device_.address();
+    publisher_ = create_publisher<autoware_perception_msgs::msg::DynamicObjectArray>("/perception/object_recognition/objects", rclcpp::QoS{10});
 
-    // std::stringstream sout;
-    // sout << mac_address_;
-    // RCLCPP_INFO(get_logger(), "MAC Address: '%s'", sout.str().c_str());
-
-    // trigger_(io_service_);
-
-    // link_layer_ = create_link_layer(io_service_, device_, "ethernet");
-    // mib_.itsGnLocalGnAddr.mid(mac_address_);
-    // mib_.itsGnLocalGnAddr.is_manually_configured(true);
-    // mib_.itsGnLocalAddrConfMethod = geonet::AddrConfMethod::Managed;
-    // mib_.itsGnSecurity = false;
-    // mib_.itsGnProtocolVersion = 1;
-
-    // context_(mib_, trigger_, *positioning_, security_.get()),
-    // context_.router_.set_address(mib_.itsGnLocalGnAddr);
-    // context_.updateMIB(mib_);
-
-    // positioning_ = create_position_provider(io_service_, trigger_.runtime());
-    // security_ = create_security_entity(trigger_.runtime(), *positioning_);
-
-    // RouterContext context_(mib_, trigger_, *positioning_, security_.get());
-    // RouterContext context_(mib_, trigger_, *positioning_, security_.get());
-    // context_.set_link_layer(link_layer_.get());
-
-    // std::unique_ptr<CpmApplication> cp_ { new CpmApplication(this) };
-    // app_ = std::move(cp_);
-
-    // context_.enable(cp_.get());
-
-    // io_service_.run();
-    // boost::thread(boost::bind(&boost::asio::io_service::run, &io_service_));
     app = new V2XApp(this);
     boost::thread v2xApp(boost::bind(&V2XApp::start, app));
-    // boost::thread v2xApp(V2XApp);
-    // boost::thread v2xApp(&V2XApp, this);
 
-    // // Print MAC Address to logger
-    // std::stringstream sout;
-    // sout << mac_address;
-    // RCLCPP_INFO(get_logger(), "MAC Address: '%s'", sout.str().c_str());
+    RCLCPP_INFO(get_logger(), "V2X Node Launched");
+  }
+
+  void V2XNode::publishObjects(std::vector<CpmApplication::Object> *objectsStack) {
+    autoware_perception_msgs::msg::DynamicObjectArray output_dynamic_object_msg;
+    std_msgs::msg::Header header;
+    rclcpp::Time current_time = this->now();
+    received_time = current_time;
+    output_dynamic_object_msg.header.frame_id = "map";
+    output_dynamic_object_msg.header.stamp = current_time;
+
+    for (CpmApplication::Object obj : *objectsStack) {
+      autoware_perception_msgs::msg::DynamicObject object;
+      autoware_perception_msgs::msg::Semantic semantic;
+      autoware_perception_msgs::msg::Shape shape;
+      autoware_perception_msgs::msg::State state;
+
+      semantic.type = autoware_perception_msgs::msg::Semantic::CAR;
+      semantic.confidence = 0.99;
+
+      shape.type = autoware_perception_msgs::msg::Shape::BOUNDING_BOX;
+      shape.dimensions.x = 2;
+      shape.dimensions.y = 5.0;
+      shape.dimensions.z = 2.5;
+
+      state.pose_covariance.pose.position.x = obj.position_x;
+      state.pose_covariance.pose.position.y = obj.position_y;
+      state.pose_covariance.pose.position.z = 0.2;
+
+      object.semantic = semantic;
+      object.shape = shape;
+      object.state = state;
+
+      output_dynamic_object_msg.objects.push_back(object);
+    }
+    cpm_received = true;
+    publisher_->publish(output_dynamic_object_msg);
   }
 
   void V2XNode::objectsCallback(const autoware_perception_msgs::msg::DynamicObjectArray::ConstSharedPtr msg)
   {
-    // RCLCPP_INFO(get_logger(), "I heard: '%s'", msg->data.c_str());
-    RCLCPP_INFO(get_logger(), "V2X: %d objects detected!", msg->objects.size());
-    // Send CPM
-    // cp_->send(msg, this, pos_lat_, pos_lon_);
-    // cp_->updateObjectsStack(msg, this);
-    app->objectsCallback(msg);
-
+    rclcpp::Time current_time = this->now();
+    // RCLCPP_INFO(get_logger(), "%ld", current_time.nanoseconds() - received_time.nanoseconds());
+    if (cpm_received) {
+      // if time difference is more than 10ms
+      if (current_time - received_time > rclcpp::Duration(std::chrono::nanoseconds(10000000))) {
+        app->objectsCallback(msg);
+      }
+      cpm_received = false;
+    } else {
+      app->objectsCallback(msg);
+    }
   }
 
   void V2XNode::tfCallback(const tf2_msgs::msg::TFMessage::ConstSharedPtr msg)
   {
-    // RCLCPP_INFO(get_logger(), "Ego Position: (%f, %f, %f)", msg->transforms[0].transform.translation.x, msg->transforms[0].transform.translation.y, msg->transforms[0].transform.translation.z);
-
     app->tfCallback(msg);
   }
 }
